903e7cf80b7a8c160db27fc71ea74c4bcc9962c2
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-06-09 21:21:09 +0530

    Added remove project service

--- internal/service/project_service.go
+++ internal/service/project_service.go
@@ -15,6 +15,7 @@ import (
 
 type IProject interface {
 	AddProject(project model.Project) error
+	RemoveProject(name string) error
 }
 
 type ProjectService struct {
@@ -65,6 +66,45 @@ func (p *ProjectService) AddProject(project model.Project) error {
 
 	if res.StatusCode != 200 {
 		return errors.New("ops! could not add project. Please try again : " + res.Status)
+	}
+
+	return nil
+}
+
+func (p *ProjectService) RemoveProject(name string) error {
+	cfg, err := config.NewAppConfig()
+	if err != nil {
+		return err
+	}
+
+	accessToken := cfg.Viper.GetString("access_token")
+
+	// Creating new http request
+	req, err := http.NewRequest("DELETE", p.config.BaseApiUrl+"/projects/remove?name="+name, nil)
+	if err != nil {
+		log.Fatal("Error creating request: ", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+
+	// Create a new HTTP client and send the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error sending request: ", err)
+	}
+
+	defer func(body io.ReadCloser) {
+		err := body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
+
+	if res.StatusCode != 200 {
+		return errors.New("ops! could not remove project. Please try again : " + res.Status)
 	}
 
 	return nil
